6d04f91bcdf194fe8778840e55d438dffb395a24
Vincente Martinez <vinny@vinnymartinez.com>
2016-11-02 02:40:01 +0000

    made app interactive

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
 # Add  code here!
+def prime?
+  print "Enter a number: "
+  integer = gets.chomp.to_i
+  prime_array = (1..integer).to_a
+  print prime_array
+  print "\n"
+  prime_count = 0
+  if integer > 0
+    prime_array.each do |pa|
+      if integer % pa == 0
+        prime_count += 1
+        puts "#{pa} <--"
+      end
+    end
+  end
+  if prime_count == 2
+    puts "this is a prime number"
+    true
+  else
+    puts "this is not a prime number"
+    false
+  end
+end
+
+prime?
